8de1d6dd8cafaef4c508883252ddaa386a0adb00
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-04-27 11:00:51 +0900

    Update MainActivity.java

--- app/src/main/java/com/coara/fwdownloader/MainActivity.java
+++ app/src/main/java/com/coara/fwdownloader/MainActivity.java
@@ -527,7 +527,7 @@ public class MainActivity extends Activity {
             final Document mergedDoc = builder.newDocument();
             Element root = mergedDoc.createElement("allDeliveryInfo");
             mergedDoc.appendChild(root);
-            String[] akamaiTypes = {"rel", "rel2", "test", "test2", "test3", "test4", "test5", "dev"};
+            String[] akamaiTypes = {"rel", "rel2", "test", "test2", "test3", "test4", "dev"};
             String[] tables = {"A", "B"};
             String[] listParams = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
             String[] infoArray = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
