31b6705aba4f454260e26d8c0a51da1defdd9ba4
コアラ <189215038+coara-chocomaru@users.noreply.github.com>
2025-04-27 10:43:55 +0900

    Update MainActivity.java

--- app/src/main/java/com/coara/fwdownloader/MainActivity.java
+++ app/src/main/java/com/coara/fwdownloader/MainActivity.java
@@ -527,10 +527,10 @@ public class MainActivity extends Activity {
             final Document mergedDoc = builder.newDocument();
             Element root = mergedDoc.createElement("allDeliveryInfo");
             mergedDoc.appendChild(root);
-            String[] akamaiTypes = {"rel", "rel2", "test", "test2", "test3", "test4", "test5", "test6", "dev"};
+            String[] akamaiTypes = {"rel", "rel2", "test", "test2", "test3", "test4", "test5", "test6", "test7", "dev", "dev2"};
             String[] tables = {"A", "B"};
             String[] listParams = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
-            String[] infoArray = {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
+            String[] infoArray = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
             for (String table : tables) {
                 List<Future<Void>> futures = new ArrayList<>();
                 for (String type : akamaiTypes) {
